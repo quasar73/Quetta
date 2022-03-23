876734969e14c03b9856a57994809b752c438eb4
Vladislav Brezgun <nextydev@gmail.com>
2022-03-23 20:51:17 +0300

    added and configured eslint and prettier

--- Web/ClientApp/src/app/app.component.ts
+++ Web/ClientApp/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'qtt-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'qtt-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ClientApp';
+    title = 'ClientApp';
 }

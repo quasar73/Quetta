039e3f36b37de351a1e770d840e4d0df2b75d2be
Vladislav Brezgun <nextydev@gmail.com>
2022-03-24 20:05:13 +0300

    configured husky and lint-staged

--- Web/ClientApp/src/app/app-routing.module.ts
+++ Web/ClientApp/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'sign-in' }];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],

f6ce5229cdf00e1a35fe29c6f805126755ac364b
Vladislav Brezgun <nextydev@gmail.com>
2022-03-25 21:40:22 +0300

    unit tests has been fixed

--- Web/ClientApp/src/app/app.component.spec.ts
+++ Web/ClientApp/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TuiModeModule, TuiRootModule, TuiThemeNightModule } from '@taiga-ui/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            imports: [RouterTestingModule],
+            imports: [RouterTestingModule, TuiRootModule, TuiThemeNightModule, TuiModeModule],
             declarations: [AppComponent],
         }).compileComponents();
     });
@@ -22,13 +23,5 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
 
         expect(app.title).toEqual('ClientApp');
-    });
-
-    it('should render title', () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        fixture.detectChanges();
-        const compiled = fixture.nativeElement as HTMLElement;
-
-        expect(compiled.querySelector('.content span')?.textContent).toContain('ClientApp app is running!');
     });
 });

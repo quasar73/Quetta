0009036b91a69e6f8bd1715b1e84e7139b6ccfea
Vladislav Brezgun <nextydev@gmail.com>
2022-04-19 17:40:36 +0300

    added footer to sidebar

--- Web/ClientApp/src/app/messenger/messenger.module.ts
+++ Web/ClientApp/src/app/messenger/messenger.module.ts
@@ -1,25 +1,31 @@
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MessengerRoutingModule } from './messenger-routing.module';
 import { MessengerComponent } from './messenger.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { TuiAvatarModule } from '@taiga-ui/kit';
+import { TuiAvatarModule, TuiDataListWrapperModule, TuiSelectModule } from '@taiga-ui/kit';
 import { SidebarHeaderComponent } from './sidebar/sidebar-header/sidebar-header.component';
-import { TuiDataListModule, TuiHostedDropdownModule, TuiSvgModule } from '@taiga-ui/core';
+import { TuiDataListModule, TuiHostedDropdownModule, TuiSvgModule, TuiTextfieldControllerModule } from '@taiga-ui/core';
 import { TranslocoModule, TRANSLOCO_SCOPE } from '@ngneat/transloco';
+import { SidebarFooterComponent } from './sidebar/sidebar-footer/sidebar-footer.component';
 
 @NgModule({
     providers: [{ provide: TRANSLOCO_SCOPE, useValue: 'messenger' }],
-    declarations: [MessengerComponent, SidebarComponent, SidebarHeaderComponent],
+    declarations: [MessengerComponent, SidebarComponent, SidebarHeaderComponent, SidebarFooterComponent],
     imports: [
         CommonModule,
         MessengerRoutingModule,
         TranslocoModule,
+        ReactiveFormsModule,
         TuiAvatarModule,
         TuiHostedDropdownModule,
         TuiDataListModule,
         TuiSvgModule,
+        TuiSelectModule,
+        TuiDataListWrapperModule,
+        TuiTextfieldControllerModule,
     ],
 })
 export class MessengerModule {}

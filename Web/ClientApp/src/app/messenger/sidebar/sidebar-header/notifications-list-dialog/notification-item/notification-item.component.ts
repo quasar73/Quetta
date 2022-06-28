1fdd4d7bbc4d780250de76293f2a13517500d07f
Vladislav Brezgun <nextydev@gmail.com>
2022-06-28 20:04:25 +0300

    fixed linter error

--- Web/ClientApp/src/app/messenger/sidebar/sidebar-header/notifications-list-dialog/notification-item/notification-item.component.ts
+++ Web/ClientApp/src/app/messenger/sidebar/sidebar-header/notifications-list-dialog/notification-item/notification-item.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'qtt-notification-item',
     templateUrl: './notification-item.component.html',
     styleUrls: ['./notification-item.component.scss'],
 })
-export class NotificationItemComponent implements OnInit {
+export class NotificationItemComponent {
     constructor() {}
-
-    ngOnInit(): void {}
 }

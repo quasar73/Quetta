0fe9d85dd5325418f389818e1ada5b091d4e6daa
Vladislav Brezgun <nextydev@gmail.com>
2022-08-14 20:37:25 +0300

    fixed linter errors

--- Quetta.Web/ClientApp/src/app/shared/services/selected-chat/selected-chat.service.spec.ts
+++ Quetta.Web/ClientApp/src/app/shared/services/selected-chat/selected-chat.service.spec.ts
@@ -3,14 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { SelectedChatService } from './selected-chat.service';
 
 describe('SelectedChatService', () => {
-  let service: SelectedChatService;
+    let service: SelectedChatService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(SelectedChatService);
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({});
+        service = TestBed.inject(SelectedChatService);
+    });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });

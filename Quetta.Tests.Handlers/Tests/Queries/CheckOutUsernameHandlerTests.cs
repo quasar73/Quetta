bc8e3b815ec706d560f08027a28183348d66de7f
Vladislav Brezgun <nextydev@gmail.com>
2022-08-28 18:24:14 +0300

    added project with unit tests for mediatr handlers, added unit tests for CheckOutUsernameHandler and GetChatInfoHandler

---
+++ Quetta.Tests.Handlers/Tests/Queries/CheckOutUsernameHandlerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Moq;
+using Quetta.Common.Models.Queries;
+using Quetta.Data.Models;
+using Quetta.Logic.Handlers.Queries;
+
+namespace Quetta.Tests.Handlers.Queries
+{
+    public class CheckOutUsernameHandlerTests
+    {
+        [Theory]
+        [InlineData("username1", true)]
+        [InlineData("username2", false)]
+        public async void CheckOutUsernameQuery_RerurnsIsUsernameAvailable(string username, bool isAvailableExpected)
+        {
+            var userManager = new Mock<UserManager<User>>(Mock.Of<IUserStore<User>>(), null, null, null, null, null, null, null, null);
+            userManager.Setup(x => x.FindByNameAsync(It.IsAny<string>())).ReturnsAsync((string username) =>
+            {
+                return username == "username1" ? new User()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    UserName = "username1",
+                    Email = "username1@mail.com"
+                } : null;
+            });
+            var handler = new CheckOutUsernameHandler(userManager.Object);
+            var query = new CheckOutUsernameQuery(username);
+
+            var isAvailable = await handler.Handle(query, new CancellationToken());
+
+            Assert.Equal(isAvailableExpected, isAvailable);
+        }
+    }
+}
---
+++ Quetta.Tests.Handlers/Tests/Queries/GetChatInfoHandlerTests.cs
@@ -0,0 +1,126 @@
+﻿using Newtonsoft.Json;
+using Quetta.Common.Exceptions;
+using Quetta.Common.Models.Queries;
+using Quetta.Common.Models.Responses;
+using Quetta.Data.Models;
+using Quetta.Logic.Handlers.Queries;
+
+namespace Quetta.Tests.Handlers.Queries
+{
+    public class GetChatInfoHandlerTests
+    {
+        private TestQuettaDbContextFactory factory;
+        private List<User> testUsers = new List<User>()
+        {
+            new User()
+            {
+                Id = "00000000-0000-0000-0000-000000000000",
+                UserName = "username",
+                FirstName = "First",
+                LastName = "Last",
+                Email = "username@email.com"
+            },
+            new User()
+            {
+                Id = "10000000-0000-0000-0000-000000000000",
+                UserName = "username2",
+                FirstName = "First",
+                LastName = "Last",
+                Email = "username2@mail.com"
+            }
+        };
+
+        public GetChatInfoHandlerTests()
+        {
+            factory = new TestQuettaDbContextFactory();
+
+            using (var dbContext = factory.Create())
+            {
+                var chat = new Chat()
+                {
+                    Id = "00000000-0000-0000-0000-000000000000",
+                    Users = testUsers,
+                    IsGroup = false,
+                };
+                dbContext.AddRange(testUsers);
+                dbContext.Add(chat);
+                dbContext.SaveChanges();
+            }
+        }
+
+        [Fact]
+        public async void GetChatInfoQuery_RerurnsChatInfo()
+        {
+            using(var dbContext = factory.Create())
+            {
+                var expectedResult = new ChatInfoResponse()
+                {
+                    IsGroup = false,
+                    Members = 2,
+                    Title = "First Last"
+                };
+                var handler = new GetChatInfoHandler(dbContext);
+                var query = new GetChatInfoQuery()
+                {
+                    ChatId = "00000000-0000-0000-0000-000000000000",
+                    UserId = "00000000-0000-0000-0000-000000000000",
+                };
+
+                var result = await handler.Handle(query, new CancellationToken());
+
+                var expectedResultStr = JsonConvert.SerializeObject(expectedResult);
+                var resultStr = JsonConvert.SerializeObject(result);
+                Assert.Equal(expectedResultStr, resultStr);
+                dbContext.Database.EnsureDeleted();
+            }
+        }
+
+        [Fact]
+        public async void GetChatInfoQueryWithInvalidChatId_ShouldThrowsEntityNotFoundException()
+        {
+            using (var dbContext = factory.Create())
+            {
+                var expectedResult = new ChatInfoResponse()
+                {
+                    IsGroup = false,
+                    Members = 2,
+                    Title = "First Last"
+                };
+                var handler = new GetChatInfoHandler(dbContext);
+                var query = new GetChatInfoQuery()
+                {
+                    ChatId = "00000000-0000-0000-0000-000000000001",
+                    UserId = "00000000-0000-0000-0000-000000000000",
+                };
+
+
+                await Assert.ThrowsAsync<EntityNotFoundException>(async () => await handler.Handle(query, new CancellationToken()));
+                dbContext.Database.EnsureDeleted();
+            }
+        }
+
+        [Fact]
+        public async void GetChatInfoQueryWithInvalidUserId_ShouldThrowsAccessDeniedException()
+        {
+            using (var dbContext = factory.Create())
+            {
+                var expectedResult = new ChatInfoResponse()
+                {
+                    IsGroup = false,
+                    Members = 2,
+                    Title = "First Last"
+                };
+                var handler = new GetChatInfoHandler(dbContext);
+                var query = new GetChatInfoQuery()
+                {
+                    ChatId = "00000000-0000-0000-0000-000000000000",
+                    UserId = "00000000-0000-0000-0000-000000000001",
+                };
+
+
+                await Assert.ThrowsAsync<AccessDeniedException>(async () => await handler.Handle(query, new CancellationToken()));
+                dbContext.Database.EnsureDeleted();
+            }
+        }
+    }
+}

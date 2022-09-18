cad1128c5c1704f9d1b3c13d92cc74e535d0dc30
Vladislav Brezgun <nextydev@gmail.com>
2022-09-18 20:36:06 +0300

    added unit test for GetInvitesHandler

--- Quetta.Tests.Handlers/Tests/Queries/GetInvitesHandlerTesting/GetInvitesHandlerTests.cs
+++ Quetta.Tests.Handlers/Tests/Queries/GetInvitesHandlerTesting/GetInvitesHandlerTests.cs
@@ -5,39 +5,37 @@ using Quetta.Logic.Handlers.Queries;
 
 namespace Quetta.Tests.Handlers.Queries
 {
-    public class GetChatsHandlerTests
+    public class GetInvitesHandlerTests
     {
         private readonly IMapper mapper;
 
-        public GetChatsHandlerTests()
+        public GetInvitesHandlerTests()
         {
             var mockMapper = new MapperConfiguration(cfg =>
             {
-                cfg.AddProfile(new ChatProfile());
+                cfg.AddProfile(new InviteProfile());
             });
             mapper = mockMapper.CreateMapper();
         }
 
         [Theory]
-        [ClassData(typeof(ChatsIsNotFoundTestCase))]
-        [ClassData(typeof(ReturnedChatsTestCase))]
-        public async void GetChats_ShouldReturnChatsList(GetChatsTestCaseModel model)
+        [ClassData(typeof(GetInvitesTestCases))]
+        public async void GetInvites_ShouldReturnsInvitesList(GetInvitesTestCaseModel model)
         {
             using (var dbContextProvider = new TestQuettaDbContextProvider())
             {
                 // Arrange
                 var dbContext = dbContextProvider.DbContext;
                 dbContext.Users.AddRange(model.UsersToAdd);
-                dbContext.Chats.AddRange(model.ChatsToAdd);
-                dbContext.Messages.AddRange(model.MessagesToAdd);
+                dbContext.Invites.AddRange(model.InvitesToAdd);
                 await dbContext.SaveChangesAsync();
-                var handler = new GetChatsHandler(dbContext, mapper);
+                var handler = new GetInvitesHandler(dbContext, mapper);
 
-                // Act
+                //Act
                 var actualResult = await handler.Handle(model.IncomingQuery, new CancellationToken());
 
                 // Assert
-                actualResult.Should().BeEquivalentTo(model.ExpectedReturnedChats);
+                actualResult.Should().BeEquivalentTo(model.ExpectedReturnedInvites);
             }
         }
     }
---
+++ Quetta.Tests.Handlers/Tests/Queries/GetInvitesHandlerTesting/GetInvitesTestCases.cs
@@ -0,0 +1,104 @@
+﻿using Quetta.Common.Enums;
+using Quetta.Common.Models.Responses;
+using Quetta.Data.Models;
+using System.Collections;
+
+namespace Quetta.Tests.Handlers.Queries
+{
+    public class GetInvitesTestCases : IEnumerable<object[]>
+    {
+        private const string ReceiverId = "00000000-0000-0000-0000-000000000000";
+        private readonly User Sender = new User()
+        {
+            Id = Guid.NewGuid().ToString(),
+            UserName = "senderusername",
+            FirstName = "First",
+            LastName = "Last"
+        };
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new GetInvitesTestCaseModel()
+                {
+                    InvitesToAdd = new Invite[]
+                    {
+                        new Invite
+                        {
+                            Id = "00000000-0000-0000-0000-000000000000",
+                            ReceiverId = ReceiverId,
+                            Status = InviteStatus.Pending,
+                            DateTime = new DateTime(2022, 01, 01),
+                            IsGroupChat = false,
+                            Sender = Sender,
+                        },
+                        new Invite
+                        {
+                            Id = "00000000-0000-0000-0000-000000000001",
+                            ReceiverId = ReceiverId,
+                            Status = InviteStatus.Pending,
+                            DateTime = new DateTime(2022, 01, 02),
+                            IsGroupChat = false,
+                            Sender = Sender,
+                        },
+                        new Invite
+                        {
+                            Id = "00000000-0000-0000-0000-000000000002",
+                            ReceiverId = ReceiverId,
+                            Status = InviteStatus.Declined,
+                            DateTime = new DateTime(2022, 01, 02),
+                            IsGroupChat = false,
+                            Sender = Sender,
+                        },
+                        new Invite
+                        {
+                            Id = "00000000-0000-0000-0000-000000000003",
+                            ReceiverId = "00000000-0000-0000-0000-000000000001",
+                            Status = InviteStatus.Pending,
+                            DateTime = new DateTime(2022, 01, 01),
+                            IsGroupChat = false,
+                            Sender = Sender,
+                        },
+                    },
+                    UsersToAdd = new User[]
+                    {
+                        Sender
+                    },
+                    IncomingQuery = new(ReceiverId),
+                    ExpectedReturnedInvites = new InviteResponse[]
+                    {
+                        new InviteResponse()
+                        {
+                            SenderUsername = "senderusername",
+                            InviteId = "00000000-0000-0000-0000-000000000000",
+                            DateTime = new DateTime(2022, 01, 01),
+                            ChatId = null,
+                            IsGroupChat = false,
+                        },
+                        new InviteResponse()
+                        {
+                            SenderUsername = "senderusername",
+                            InviteId = "00000000-0000-0000-0000-000000000001",
+                            DateTime = new DateTime(2022, 01, 02),
+                            ChatId = null,
+                            IsGroupChat = false,
+                        },
+                    }
+                }
+            };
+            yield return new object[]
+            {
+                new GetInvitesTestCaseModel()
+                {
+                    InvitesToAdd = Array.Empty<Invite>(),
+                    UsersToAdd =  Array.Empty<User>(),
+                    IncomingQuery = new(ReceiverId),
+                    ExpectedReturnedInvites = Array.Empty<InviteResponse>(),
+                }
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}

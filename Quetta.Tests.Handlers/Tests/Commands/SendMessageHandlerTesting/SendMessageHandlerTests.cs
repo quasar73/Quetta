7bbfd74315c2d9dd2fd88f454c03a292e2bb9d99
Vladislav Brezgun <nextydev@gmail.com>
2022-09-22 14:24:33 +0300

    added unit tests for SendMessageHandler

---
+++ Quetta.Tests.Handlers/Tests/Commands/SendMessageHandlerTesting/SendMessageHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Quetta.Logic.Handlers.Commands;
+
+namespace Quetta.Tests.Handlers.Commands
+{
+    public class SendMessageHandlerTests
+    {
+        [Theory]
+        [ClassData(typeof(SendMessageTestCase))]
+        public async void SendMessage_ShouldCreateNewMessage(SendMessageTestCaseModel model)
+        {
+            using(var dbContextProvider = new TestQuettaDbContextProvider())
+            {
+                // Arrange
+                var dbContext = dbContextProvider.DbContext;
+                var handler = new SendMessageHandler(dbContext);
+
+                // Act
+                var id = await handler.Handle(model.IncomingCommand, new CancellationToken());
+                var message = dbContext.Messages.FirstOrDefault(m => m.Id == id);
+
+                // Assert
+                message.Should().NotBeNull();
+                model.ExpectedResult.Should().BeEquivalentTo(message, options => options
+                    .Excluding(m => m!.Id)
+                    .Excluding(m => m!.Chat)
+                    .Excluding(m => m!.User));
+            }
+        }
+    }
+}
---
+++ Quetta.Tests.Handlers/Tests/Commands/SendMessageHandlerTesting/SendMessageTestCase.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace Quetta.Tests.Handlers.Commands
+{
+    public class SendMessageTestCase : IEnumerable<object[]>
+    {
+        private const string SendreId = "00000000-0000-0000-0000-000000000000";
+        private const string ChatId = "00000000-0000-0000-0000-000000000000";
+        private const string TestText = "This is test text";
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[]
+            {
+                new SendMessageTestCaseModel()
+                {
+                    IncomingCommand = new()
+                    {
+                        SenderId = SendreId,
+                        ChatId = ChatId,
+                        Date = new DateTime(2022, 01, 01),
+                        Text = TestText,
+                    },
+                    ExpectedResult = new()
+                    {
+                         ChatId = ChatId,
+                         UserId = SendreId,
+                         Date = new DateTime(2022, 01, 01),
+                         Text = TestText,
+                    }
+                },
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}

71f1e5039e0b9409b858964bb572c1b9b2be2962
Vladislav Brezgun <nextydev@gmail.com>
2022-11-16 22:21:59 +0300

    added validator and tests for getmessagesrequest

---
+++ Quetta.Common/Validators/Requests/GetMessagesValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Quetta.Common.Models.Requests;
+
+namespace Quetta.Common.Validators.Requests
+{
+    public class GetMessagesValidator : AbstractValidator<GetMessagesRequest>
+    {
+        public GetMessagesValidator()
+        {
+            RuleFor(model => model.ChatId).NotEmpty();
+            RuleFor(model => model.Amount).GreaterThan(0);
+        }
+    }
+}
---
+++ Quetta.Tests.Validators/Tests/Requests/GetMessagesValidatorTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentValidation.TestHelper;
+using Quetta.Common.Models.Requests;
+using Quetta.Common.Validators.Requests;
+
+namespace Quetta.Tests.Validators.Requests
+{
+    public class GetMessagesValidatorTests
+    {
+        private readonly GetMessagesValidator validator = new GetMessagesValidator();
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        [InlineData(null)]
+        public void GivenAnInvalidChatIdValue_ShouldHaveValidationError(string chatId)
+        {
+            // Arrange
+            var model = new GetMessagesRequest() { ChatId = chatId };
+
+            // Act
+            var result = validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(model => model.ChatId);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(-1)]
+        [InlineData(-999)]
+        public void GivenAnInvalidAmountValue_ShouldHaveValidationError(int amount)
+        {
+            // Arrange
+            var model = new GetMessagesRequest() { Amount = amount };
+
+            // Act
+            var result = validator.TestValidate(model);
+
+            // Assert
+            result.ShouldHaveValidationErrorFor(model => model.Amount);
+        }
+
+        [Fact]
+        public void GivenValidValues_ShouldNotHaveValidationError()
+        {
+            // Act
+            var model = new GetMessagesRequest()
+            {
+                ChatId = "somechatid",
+                Amount = 1,
+            };
+
+            // Act
+            var result = validator.TestValidate(model);
+
+            // Assert
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
